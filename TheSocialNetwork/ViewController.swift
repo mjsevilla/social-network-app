1143bdd7f8246213bfcea638bea60656e560c3ee
Mike Sevilla <michaelsevilla@gmail.com>
2017-04-28 09:33:42 -0700

    Initial Commit

---
+++ TheSocialNetwork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheSocialNetwork
+//
+//  Created by Mike Sevilla on 4/28/17.
+//  Copyright © 2017 Mike Sevilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
